5cecc50a7c51e6f1d54cbc3783c59e78f89b7af0
Gichan,Kim <kgichan12@gmail.com>
2019-07-01 11:51:32 +0900

    update data with firebase

---
+++ updateDataWithFirebase.py
@@ -0,0 +1,26 @@
+import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from mcp3208 import MCP3208
+
+
+adc=MCP3208()
+
+cred = credentials.Certificate("../haniumnoisecheckapp-firebase-adminsdk-bov3x-d0d71d795f.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://haniumnoisecheckapp.firebaseio.com/'
+})
+
+ref = db.reference('/noise/id')
+ref_id = ref.child('1')
+
+
+while True:
+    tmp= adc.read(0)
+    if( tmp > 30 ):
+        ref_id.push({
+            'time': time.ctime(),
+            'vib':tmp,
+        })
+        print("uploaded complete")
